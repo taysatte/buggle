e31e91d502176ef8d4391d072ad387f6b273d03b
taysatte <taylor.sattenfield@gmail.com>
2025-03-09 23:48:24 -0500

    added api call to execute code

---
+++ src/app/api/api.ts
@@ -0,0 +1,29 @@
+export const executeCode = async (language: string, sourceCode: string) => {
+  try {
+    const response = await fetch("https://emkc.org/api/v2/piston/execute", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        language: language,
+        version: "18.15.0",
+        files: [
+          {
+            content: sourceCode,
+          },
+        ],
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error executing code:", error);
+    throw error; // Rethrow the error to be handled by the caller.
+  }
+};
