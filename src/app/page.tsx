2c1d08b42fb572da096178f8041dc6df75cfebf3
taysatte <taylor.sattenfield@gmail.com>
2025-03-21 23:39:14 -0500

    code submit button refactored as an independent component outside of code editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,85 @@
 "use client";
 
+import React, { useRef, useState } from "react";
 import CodeEditor from "@/components/CodeEditor";
 import { Card } from "@/components/ui/card";
 import { TEST_CHALLENGES, THEME_BASE, TEST_LANG } from "@/constants";
+import EditorControls from "@/components/EditorControls";
+import Output from "@/components/Output"; // Import Output
 
 export default function App() {
   const theme = THEME_BASE;
   const challenge = TEST_CHALLENGES[0];
   const language = TEST_LANG;
 
+  const [sourceCode, setSourceCode] = useState("");
+  const [output, setOutput] = useState<string[]>([""]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [testsPassed, setTestsPassed] = useState<boolean | null>(null);
+
+  const runCode = async () => {
+    if (!sourceCode) return;
+    try {
+      setIsLoading(true);
+      setTestsPassed(null);
+      const response = await fetch("/api/execute", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          language,
+          sourceCode,
+          challengeId: challenge.id,
+        }),
+      });
+      if (!response.ok)
+        throw new Error(`HTTP error! status: ${response.status}`);
+      const data = await response.json();
+      setOutput(data.pistonResult.run.output.split("\n"));
+      setTestsPassed(data.testsPassed);
+    } catch (error) {
+      console.error(error);
+      setOutput(["An error occurred during execution."]);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col w-full h-full gap-4 px-4">
-        {/** TODO: editor controls component */}
         <div className="w-full h-[64px]">
-          <Card className="w-full h-full flex justify-center items-center text-primary/50 bg-secondary/50">{`{ Editor Controls }`}</Card>
-        </div>
-        <div className="flex flex-row w-full h-full gap-4">
-          <div className="w-2/3 h-full">
-            <CodeEditor
-              challenge={challenge}
-              theme={theme}
+          <Card className="w-full h-full bg-secondary/50 flex items-center justify-center p-4">
+            <EditorControls
+              sourceCode={sourceCode}
               language={language}
+              challengeId={challenge.id}
+              runCode={runCode}
+              isLoading={isLoading}
             />
+          </Card>
+        </div>
+        <div className="flex flex-row w-full h-[calc(100vh-160px)] gap-4">
+          <div className="w-7/12 h-full">
+            <Card className="w-full h-full flex flex-col">
+              <div className="w-full h-3/4">
+                <CodeEditor
+                  onCodeChange={setSourceCode}
+                  theme={theme}
+                  challenge={challenge}
+                  language={language}
+                />
+              </div>
+              <div className="w-full h-1/4 border-t-2">
+                <Output
+                  output={output}
+                  isLoading={isLoading}
+                  testsPassed={testsPassed}
+                />
+              </div>
+            </Card>
           </div>
-          {/** TODO: content section component */}
-          <div className="w-1/3 h-[calc(100vh-160px)]">
+          {/* TODO: content section somponent */}
+          <div className="w-5/12 h-[calc(100vh-160px)]">
             <Card className="w-full h-full flex justify-center items-center text-primary/50 bg-secondary/50">
               {`{ Content Section }`}
             </Card>
--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -1,94 +1,37 @@
-import { editor } from "monaco-editor";
-import IEditor = editor.IEditor;
-import React, { RefObject, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Loader2, SendHorizonal } from "lucide-react";
+import React from "react";
 import { ScrollArea } from "./ui/scroll-area";
+import { Loader2 } from "lucide-react";
 
-type OutputProps = {
-  editorRef: RefObject<IEditor | null>;
-  language: string;
-  challengeId: string;
-};
+interface OutputProps {
+  output: string[];
+  isLoading: boolean;
+  testsPassed: boolean | null;
+}
 
-const Output = ({ editorRef, language, challengeId }: OutputProps) => {
-  const [output, setOutput] = useState<string[]>([""]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [testsPassed, setTestsPassed] = useState<boolean | null>(null);
-
-  const runCode = async () => {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-
-    const sourceCode = editorRef.current.getValue();
-    if (!sourceCode) return;
-    try {
-      setIsLoading(true);
-      setTestsPassed(null); // Reset test results
-      const response = await fetch("/api/execute", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          language,
-          sourceCode,
-          challengeId,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      const pistonOutput = data.pistonResult.run.output;
-      setOutput(pistonOutput.split("\n"));
-      setTestsPassed(data.testsPassed);
-    } catch (error) {
-      console.error(error);
-      setOutput(["An error occurred during execution."]);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
+const Output = ({ output, isLoading, testsPassed }: OutputProps) => {
   return (
-    <>
-      <div className="flex flex-col w-full h-[250px] border-t-2 border-b-2">
-        <ScrollArea className="h-[250px] w-full py-4 px-6 font-mono-default text-[1rem]">
-          {isLoading ? (
-            <div className="flex items-center justify-center h-full">
-              <Loader2 className="animate-spin h-6 w-6 mr-2" /> loading...
-            </div>
-          ) : (
-            <div className="text-muted-foreground">
-              {output.map((line, i) => (
-                <div key={i}>{`> buggle@user ~> ${line}`}</div>
-              ))}
-              {testsPassed !== null && (
-                <div
-                  className={`${testsPassed ? "text-green-500" : "text-destructive-foreground"}`}
-                >
-                  {testsPassed ? "tests passed!" : "tests failed!"}
-                </div>
-              )}
+    <ScrollArea className="h-full w-full py-4 px-6 font-mono-default text-[1rem]">
+      {isLoading ? (
+        <div className="flex items-center justify-center h-full">
+          <Loader2 className="animate-spin h-6 w-6 mr-2" /> loading...
+        </div>
+      ) : (
+        <div className="text-muted-foreground">
+          {output.map((line, i) => (
+            <div key={i}>{`> buggle@user ~> ${line}`}</div>
+          ))}
+          {testsPassed !== null && (
+            <div
+              className={`${
+                testsPassed ? "text-green-500" : "text-destructive-foreground"
+              }`}
+            >
+              {testsPassed ? "tests passed!" : "tests failed!"}
             </div>
           )}
-        </ScrollArea>
-      </div>
-      <div className="flex w-full h-auto items-center justify-center mb-4">
-        <Button
-          className="cursor-pointer w-fit border-2 text-[1rem] font-mono-default p-6"
-          variant="outline"
-          onClick={runCode}
-          disabled={isLoading}
-        >
-          <SendHorizonal className="w-4 h-4" />
-          {isLoading ? "loading..." : "submit"}
-        </Button>
-      </div>
-    </>
+        </div>
+      )}
+    </ScrollArea>
   );
 };
 
