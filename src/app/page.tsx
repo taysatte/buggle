ca7c6755287f43537cd50e2333d28d15c46d45bf
taysatte <taylor.sattenfield@gmail.com>
2025-10-15 12:08:55 -0500

    shadcn component cli hooked up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const handleButtonClick = () => {
+    console.log("Button clicked!");
+  };
+
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex min-h-screen items-center justify-center p-24 bg-background">
+      <Button className="cursor-pointer" onClick={handleButtonClick}>
+        Testing
+      </Button>
     </div>
   );
 }
