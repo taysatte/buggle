13b1994220148c6893ea8fd3a94fa2f9b645d57e
taysatte <taylor.sattenfield@gmail.com>
2025-10-15 21:54:30 -0500

    refactor: simplify global PrismaClient initialization and improve type safety

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,13 @@
 import { PrismaClient } from "../generated/prisma";
 
-declare global {
-  // allow global `var` for dev hot-reloading
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  global.prisma || (global.prisma = new PrismaClient({ log: ["query"] }));
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
