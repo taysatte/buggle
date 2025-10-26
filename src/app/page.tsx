96650fe6cd27117862d010dbd423142d570a40d1
taysatte <taylor.sattenfield@gmail.com>
2025-10-25 22:23:40 -0500

    remove 'allotment' package from dependencies in package.json and update related pnpm-lock.yaml entries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@
 
 import CodeEditor from "@/components/editor/CodeEditor";
 import Navbar from "@/components/navbar/Navbar";
-import { Allotment } from "allotment";
 import { Card } from "@/components/ui/card";
-import "allotment/dist/style.css";
 import Output from "@/components/output/Output";
 import {
   Collapsible,
@@ -18,42 +16,22 @@ const PuzzlePage = () => {
       <Navbar />
       {/* Main content area */}
       <main className="h-[calc(100vh-64px)] px-4 pb-4">
-        <div className="allotment-theme h-full">
-          <Allotment className="h-full" defaultSizes={[65, 35]} minSize={100}>
-            {/* Code Editor */}
-            <Allotment
-              className="h-full"
-              defaultSizes={[90, 10]}
-              vertical
-              minSize={100}
-            >
-              {/* Code Editor */}
-              <Allotment.Pane minSize={100}>
-                <Card className="rounded-3xl h-[calc(100%-6px)] w-[calc(100%-6px)] shadow-md">
-                  <CodeEditor />
-                </Card>
-              </Allotment.Pane>
-              {/* Output Window */}
-              <Allotment.Pane minSize={100}>
-                <Card className="h-[calc(100%-6px)] px-4 rounded-3xl w-[calc(100%-6px)] mt-1.5 flex justify-start items-start shadow-md">
-                  <Collapsible>
-                    <CollapsibleTrigger>Toggle Output</CollapsibleTrigger>
-                    <CollapsibleContent>
-                      <Output
-                        output={[]}
-                        isLoading={false}
-                        testsPassed={null}
-                      />
-                    </CollapsibleContent>
-                  </Collapsible>
-                </Card>
-              </Allotment.Pane>
-            </Allotment>
-            {/* Puzzle Description */}
-            <Allotment.Pane minSize={100}>
-              <Card className="h-full w-[calc(100%-6px)] rounded-3xl ml-1.5 shadow-md"></Card>
-            </Allotment.Pane>
-          </Allotment>
+        <div className="h-full">
+          {/* Code Editor */}
+          <Card className="rounded-3xl h-[calc(100%-6px)] w-[calc(100%-6px)] shadow-md">
+            <CodeEditor />
+          </Card>
+          {/* Output Window */}
+          <Card className="h-[calc(100%-6px)] px-4 rounded-3xl w-[calc(100%-6px)] mt-1.5 flex justify-start items-start shadow-md">
+            <Collapsible>
+              <CollapsibleTrigger>Toggle Output</CollapsibleTrigger>
+              <CollapsibleContent>
+                <Output output={[]} isLoading={false} testsPassed={null} />
+              </CollapsibleContent>
+            </Collapsible>
+          </Card>
+          {/* Puzzle Description */}
+          <Card className="h-full w-[calc(100%-6px)] rounded-3xl ml-1.5 shadow-md"></Card>
         </div>
       </main>
     </>
