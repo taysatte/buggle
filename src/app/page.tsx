dddb9c09b0511efb8beb46d1ee0a43254135135e
taysatte <taylor.sattenfield@gmail.com>
2025-10-25 22:28:20 -0500

    update dependencies in package.json and pnpm-lock.yaml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,20 +18,17 @@ const PuzzlePage = () => {
       <main className="h-[calc(100vh-64px)] px-4 pb-4">
         <div className="h-full">
           {/* Code Editor */}
-          <Card className="rounded-3xl h-[calc(100%-6px)] w-[calc(100%-6px)] shadow-md">
+          <Card className="shadow-md h-full w-full">
             <CodeEditor />
           </Card>
           {/* Output Window */}
-          <Card className="h-[calc(100%-6px)] px-4 rounded-3xl w-[calc(100%-6px)] mt-1.5 flex justify-start items-start shadow-md">
-            <Collapsible>
-              <CollapsibleTrigger>Toggle Output</CollapsibleTrigger>
-              <CollapsibleContent>
-                <Output output={[]} isLoading={false} testsPassed={null} />
-              </CollapsibleContent>
-            </Collapsible>
+          <Card className="h-full w-full shadow-md">
+            <Output output={[]} isLoading={false} testsPassed={null} />
           </Card>
           {/* Puzzle Description */}
-          <Card className="h-full w-[calc(100%-6px)] rounded-3xl ml-1.5 shadow-md"></Card>
+          <Card className="h-full w-full shadow-md">
+            <p>Puzzle Description</p>
+          </Card>
         </div>
       </main>
     </>
